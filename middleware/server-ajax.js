169c4b8ff8515e0bb4a6542c7a8e1cda17c831b8
yt-wsl <ytc314a@gmai.com>
2022-04-23 15:40:08 +0800

    changed global var players' structure

--- middleware/server-ajax.js
+++ middleware/server-ajax.js
@@ -30,7 +30,7 @@ module.exports = function (app) {
 		console.log("AJAX: " + username + " request to match");
 
 		// cannot play if two players are already playing
-		if (players.player1 && players.player2) {
+		if (Object.keys(players).length == 2) {
 			res.json({
 				status: "error",
 				error: "there are already two players playing, please wait",
@@ -38,19 +38,8 @@ module.exports = function (app) {
 			return;
 		}
 
-		// if this is the first player, then simply add to player list
-		if (!players.player1 && !players.player2) {
-			res.json({
-				status: "success",
-				success: "queue",
-			});
-			return;
-		}
-
-		// this is the second player, start the game immediately
 		res.json({
 			status: "success",
-			success: "start",
 		});
 	});
 };
--- middleware/server-socket.js
+++ middleware/server-socket.js
@@ -11,16 +11,12 @@ module.exports = function (io) {
 			if (socket.request.session.user) {
 				const { username } = socket.request.session.user;
 				console.log("Socket: added " + username + " to queue");
-				if (!players.player1) {
-					players.player1 = username;
-				} else {
-					players.player2 = username;
-					// all players present => start the game
+				players[username] = [];
+				if (Object.keys(players).length == 2) {
 					io.emit("start game");
+					// TODO: server-side start the game
+					console.log("game started " + JSON.stringify(players));
 				}
-
-				// TODO: server-side start the game
-				console.log("game started " + players);
 			}
 		});
 	});
