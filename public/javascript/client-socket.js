ed362778f10ad0b6e76359a589a7a90972a85bde
Savio666 <savio200172@yahoo.com.hk>
2022-04-26 17:06:04 +0800

    Initiate first turn

--- public/javascript/client-socket.js
+++ public/javascript/client-socket.js
@@ -19,6 +19,8 @@ const Socket = (function () {
 		socket.on("start game", () => {
 			// TODO: client-side start game things
 			WaitingScreen.hide();
+			$("#waiting").hide();
+			$("#yourTurn").hide();
 			console.log("game started");
 		});
 	};
@@ -26,14 +28,40 @@ const Socket = (function () {
 	const draw_card = function () {
 		if(firstDraw == true){
 			socket.emit("firstDraw")
+			$("#test-button").hide()
 			firstDraw = false
 		}
 		else{
 			socket.emit("draw")
 		}
-		socket.on("card drawn", (cards) => {
-			Game.initialize(cards);
-			Game.renderOpponentCard(cards.length);
+		socket.on("firstdrawn", (cardsObject) => {
+			console.log(cardsObject)
+			let myCards = cardsObject.cards
+			let myOpponentLength  = cardsObject.opponentLength
+			Game.initialize(myCards);
+			if(myOpponentLength==0){
+				$("#waiting").show()
+			}else{
+				socket.emit("Both drawn");
+			}
+			Game.renderOpponentCard(myOpponentLength);
+		})
+
+		socket.on("InitiateTurns",(usernameAndOppolength)=>{
+			if(usernameAndOppolength.myName==Authentication.getUser().username){
+				Game.renderOpponentCard(usernameAndOppolength.opponentLength)
+				$("#test-button").show();
+				$("#yourTurn").show();
+				$("#waiting").hide();
+			}
+			// opponent
+			else{
+				Game.renderOpponentCard(usernameAndOppolength.opponentLength)
+				$("#test-button").hide();
+				$("#yourTurn").hide();
+				$("#waiting").show()
+			}
+
 		})
 	}
 
