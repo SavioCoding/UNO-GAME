ab93678ad3ab55ad96e81943d130a11ee82a0764
Savio666 <savio200172@yahoo.com.hk>
2022-04-30 00:11:55 +0800

    Write some comments

--- public/javascript/client-socket.js
+++ public/javascript/client-socket.js
@@ -1,11 +1,16 @@
 const Socket = (function () {
 	let socket = null;
+
+	// a gate for determining 1st draw or later draws
 	let firstDraw = true;
+
+	// number of cards remaining in the opponent's deck
 	let myOpponentLength = 0;
 
 	const getSocket = function () {
 		return socket;
 	};
+
 
 	const connect = function () {
 		socket = io();
@@ -14,7 +19,91 @@ const Socket = (function () {
 			console.log("Connected");
 		});
 
-		// opponent drawn
+		/*
+		It is the first time the users draw the card, server send the cards drawn back to user, 
+		wait if the opponet still drawing, else send to the server than both are drawn,
+		such that server can choose which player starts first
+		*/
+		socket.on("firstdrawn", (cardsObject) => {
+			let myCards = cardsObject.cards;
+			let myOpponentLength = cardsObject.opponentLength;
+			Game.initialize(myCards);
+			if (myOpponentLength == 0) {
+				$("#waiting").show();
+			} else {
+				socket.emit("Both drawn");
+			}
+			Game.renderOpponentCard(myOpponentLength);
+		});
+
+		/* At the first turn, server received socket.emit("Both drawn") from socket.on("firstdrawn"),
+		It decide which player to start first randomly, io.emit("InitiateTurns") is called from server. In the response
+		passed, there is the name and this name determine which user to start first.
+		*/
+		socket.on("InitiateTurns", (res) => {
+			// It is my turn now
+			if (res.myName == Authentication.getUser().username) {
+				myOpponentLength = res.opponentLength;
+				Game.renderOpponentCard(res.opponentLength);
+				$("#test-button").show();
+				$("#yourTurn").show();
+				$("#waiting").hide();
+				Game.changeTurn(true);
+				Timer.startTimer();
+			}
+			// It is opponent's turn now
+			else {
+				myOpponentLength = res.opponentLength;
+				Game.renderOpponentCard(res.opponentLength);
+				$("#test-button").hide();
+				$("#yourTurn").hide();
+				$("#waiting").show();
+			}
+			Game.PutCard(res.lastCard);
+			$("#timer").show();
+		});
+
+		/* Card is being checked in the server, user then know if the card is
+		valid or not from this function, if it is valid, put the checked card's
+		id in Game.checkedCard variable, so that if it is clicked again, 
+		use card will be used
+		*/
+		socket.on("cardChecked", (res) => {
+			if (res["valid"]) {
+				Game.changeCheckedCard(res["id"]);
+				$("#invalidCard").hide();
+				$("#validCard").show();
+			} else {
+				$("#validCard").hide();
+				$("#invalidCard").show();
+			}
+		});
+
+		/* Current user draw a card (not the first turn) or from opponent's +2/+4
+		the remaining deck is sent back, the current user then render the remaining
+		deck in his UI, it is opponent's turn if I draw it, it is my turn if opponent
+		use +2/+4 on me. (+4 is not included here as the changed turn is handled
+		when changing color)
+		*/
+		socket.on("card drawn", (res) => {
+			Game.initialize(res["cards"]);
+			// normal draw
+			if (res.number == 1) {
+				changeToOpponentTurn();
+				// +2 or +4
+			} else if (res.number == 2){
+				changeToMyTurn();
+			}
+			$("#changeColor").hide();
+		});
+
+		/* The opponent draw a card (not the first turn) or from opponent's +2/+4,
+		the number of cards the opponent drawn is sent back and rendered.
+		If opponent draw 1, it becomes my turn
+		If I use +2/+4 on my opponent, it becomes my opponent's turn
+		(+4 is not included here as the changed turn is handled
+		when changing color)
+		*/
 		socket.on("opponent drawn", (number) => {
 			myOpponentLength += number;
 			Game.renderOpponentCard(myOpponentLength);
@@ -28,65 +117,13 @@ const Socket = (function () {
 			}
 		});
 
-		socket.on("firstdrawn", (cardsObject) => {
-			let myCards = cardsObject.cards;
-			let myOpponentLength = cardsObject.opponentLength;
-			Game.initialize(myCards);
-			if (myOpponentLength == 0) {
-				$("#waiting").show();
-			} else {
-				socket.emit("Both drawn");
-			}
-			Game.renderOpponentCard(myOpponentLength);
-		});
-
-		// current User drawn
-		socket.on("card drawn", (res) => {
-			Game.initialize(res["cards"]);
-			// normal draw
-			if (res.number == 1) {
-				changeToOpponentTurn();
-				// +2 or +4
-			} else if (res.number == 2){
-				changeToMyTurn();
-			}
-			$("#changeColor").hide();
-		});
-
-		socket.on("InitiateTurns", (res) => {
-			//me
-			if (res.myName == Authentication.getUser().username) {
-				myOpponentLength = res.opponentLength;
-				Game.renderOpponentCard(res.opponentLength);
-				$("#test-button").show();
-				$("#yourTurn").show();
-				$("#waiting").hide();
-				Game.changeTurn(true);
-				Timer.startTimer();
-			}
-			// opponent
-			else {
-				myOpponentLength = res.opponentLength;
-				Game.renderOpponentCard(res.opponentLength);
-				$("#test-button").hide();
-				$("#yourTurn").hide();
-				$("#waiting").show();
-			}
-			Game.PutCard(res.lastCard);
-			$("#timer").show();
-		});
-		socket.on("cardChecked", (res) => {
-			if (res["valid"]) {
-				Game.changeCheckedCard(res["id"]);
-				$("#invalidCard").hide();
-				$("#validCard").show();
-			} else {
-				$("#validCard").hide();
-				$("#invalidCard").show();
-			}
-		});
-
-		// I used the card
+		/* I used a card
+		call useCardAndPut() to put the card in the middle of the gameboard
+		If it is Ban/Swap, it is my turn again
+		If it is change color or add 4, let the user to pick the color
+		If it is a normal card, change the turn to your opponent
+		If it is add 2, do nothing, as opponent drawn/ card drawn will handle it
+		*/
 		socket.on("card used", (res) => {
 			Game.useCardAndPut(res["id"], res["cards"]);
 			if (res["special"] === "Ban" || res["special"] === "Swap") {
@@ -100,7 +137,13 @@ const Socket = (function () {
 			$("#changeColor").hide();
 		});
 
-		// opponent use the card
+		/*
+		Opponent use a card
+		call useCardAndPut() to put the card in the middle of the gameboard
+		If it is an add 2 or add 4, add the corresponding number of cards to me
+		If it is Ban/Swap, it is opponent's turn again
+		If it is a normal card, change to my turn now
+		*/
 		socket.on("opponent used", (res) => {
 			Game.PutCard(res["lastCard"]);
 			myOpponentLength -= 1;
@@ -113,12 +156,15 @@ const Socket = (function () {
 			}
 			else if (res["special"] === "Ban" || res["special"] === "Swap") {
 				changeToOpponentTurn();
-			} else if(res["special"] !== null) {
+			} else if(res["special"] === null) {
 				changeToMyTurn();
 			}
 		});
 
-		// opponent changed color
+		/* opponent changed color (+4/change color)
+		The server sent me the color that opponent changed from (+4/change color)
+		Show the corresponding message and changed to my turn
+		*/
 		socket.on("opponent changed color", (color) => {
 			$("#changeColor").text(
 				"Your opponent changed the color to " + color
@@ -154,6 +200,8 @@ const Socket = (function () {
 		});
 	};
 
+	/* the draw card button call this function, 
+	use variable firstDraw to determine whether this is actually the first time to draw */
 	const draw_card = function () {
 		if (firstDraw == true) {
 			socket.emit("firstDraw");
@@ -164,14 +212,19 @@ const Socket = (function () {
 		}
 	};
 
+	/* If it is the first time to press the card, emit the checkCard event to the server */
 	const checkCard = function (id) {
 		socket.emit("checkCard", id);
 	};
 
+	/* Use the card if the card is valid */
 	const useCard = function (id) {
 		socket.emit("useCard", id);
 	};
 
+	/* The opponent finished his turn and 
+	it is my turn now */
+	// By changing button and displaying message
 	const changeToMyTurn = function () {
 		$("#yourTurn").show();
 		$("#waiting").hide();
@@ -181,6 +234,8 @@ const Socket = (function () {
 		Timer.startTimer();
 	};
 
+	/* I finished my turn and it is opponent's turn */
+	// By changing button and displaying message
 	const changeToOpponentTurn = function () {
 		$("#yourTurn").hide();
 		$("#waiting").show();
@@ -192,7 +247,11 @@ const Socket = (function () {
 		Timer.stopTimer();
 	};
 
-	// for case with change color or +4
+	/* the red/green/blue/yellow buttons called it (For +4/change color),
+	such and this function notify the server which color
+	the current user want to change, then change to opponent's turn 
+	after selecting the color
+	*/
 	const changeColor = (color) => {
 		SelectColorScreen.hide();
 		GameScreen.show();
@@ -200,6 +259,7 @@ const Socket = (function () {
 		changeToOpponentTurn();
 	};
 
+	/* called from timer.js to inform the server that time is up and game is over */
 	const timesUp = () => {
 		socket.emit("times up")
 	}
